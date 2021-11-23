a2e96b65f1fa5cd91ede06e5c6763891c9295c81
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2021-11-23 14:30:14 +0100

    Update libddwaf to 1.0.14

--- lib/datadog/security/waf/version.rb
+++ lib/datadog/security/waf/version.rb
@@ -2,7 +2,7 @@ module Datadog
   module Security
     module WAF
       module VERSION
-        BASE_STRING = '1.0.13'
+        BASE_STRING = '1.0.14'
         STRING = "#{BASE_STRING}.0.0.beta1"
         MINIMUM_RUBY_VERSION = '2.1'
         MAXIMUM_RUBY_VERSION = '3.2'
