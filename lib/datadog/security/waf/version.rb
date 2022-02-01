be20f36d5f255b591021f18b806edb9944b6d71f
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2022-02-01 16:30:29 +0100

    Relax maximum version

--- lib/datadog/security/waf/version.rb
+++ lib/datadog/security/waf/version.rb
@@ -5,7 +5,6 @@ module Datadog
         BASE_STRING = '1.0.14'
         STRING = "#{BASE_STRING}.1.0.beta1"
         MINIMUM_RUBY_VERSION = '2.1'
-        MAXIMUM_RUBY_VERSION = '3.2'
       end
     end
   end
