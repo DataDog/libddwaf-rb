5dd11665bc5c65ec68e9c87ab6618f01a4001eba
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2022-05-20 23:58:56 +0200

    Release v1.3.0.2.0

--- lib/datadog/appsec/waf/version.rb
+++ lib/datadog/appsec/waf/version.rb
@@ -3,7 +3,7 @@ module Datadog
     module WAF
       module VERSION
         BASE_STRING = '1.3.0'
-        STRING = "#{BASE_STRING}.1.0"
+        STRING = "#{BASE_STRING}.2.0"
         MINIMUM_RUBY_VERSION = '2.1'
       end
     end
