acec71ce6c429e468d587ecd07ac02fce48142ba
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2022-07-27 17:30:19 +0200

    Use a method+ivar instead of a lambda+closure for logging

--- lib/datadog/appsec/waf.rb
+++ lib/datadog/appsec/waf.rb
@@ -409,26 +409,34 @@ module Datadog
         end
       end
 
+      def self.log_callback(level, func, file, line, message, len)
+        return if logger.nil?
+
+        logger.debug do
+          {
+            level: level,
+            func: func,
+            file: file,
+            line: line,
+            message: message.read_bytes(len)
+          }.inspect
+        end
+      end
+
+      def self.logger
+        @logger
+      end
+
       def self.logger=(logger)
-        # @type var log_cb: ^(::Symbol, ::String, ::String, ::Integer, ::FFI::Pointer, ::Integer) -> void
-        log_cb = lambda do |level, func, file, line, message, len|
-          # @type var message: ::FFI::Pointer
-          logger.debug do
-            {
-              level: level,
-              func: func,
-              file: file,
-              line: line,
-              message: message.read_bytes(len)
-            }.inspect
-          end
+        unless @log_callback
+          log_callback = method(:log_callback)
+          Datadog::AppSec::WAF::LibDDWAF.ddwaf_set_log_cb(log_callback, :ddwaf_log_trace)
+
+          # retain logging proc if set properly
+          @log_callback = log_callback
         end
 
-        Datadog::AppSec::WAF::LibDDWAF.ddwaf_set_log_cb(log_cb, :ddwaf_log_trace)
-
-        # retain. also, assign late to prevent old @log_cb being GC'd while
-        # still in use C side before the new one is set
-        @log_cb = log_cb
+        @logger = logger
       end
 
       class Handle
