ebf14ce5126a6b32cb355ff592200133cf90f717
Gustavo Caso <gustavo.caso@datadoghq.com>
2023-02-10 10:01:07 +0100

    improve specs messages

--- spec/datadog/appsec/waf_spec.rb
+++ spec/datadog/appsec/waf_spec.rb
@@ -234,10 +234,10 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
     end
 
     context 'getters' do
-      let(:object_for_getter_spec) { libddwaf::Object.new }
+      let(:ddwaf_object) { libddwaf::Object.new }
 
       after do
-        libddwaf.ddwaf_object_free(object_for_getter_spec)
+        libddwaf.ddwaf_object_free(ddwaf_object)
       end
 
       describe '.ddwaf_object_type' do
@@ -248,16 +248,16 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
           ['for unsigened object', :ddwaf_object_unsigned_force, 12, :ddwaf_obj_unsigned],
           ['for string object', :ddwaf_object_string, "Hello World", :ddwaf_obj_string],
           ['for boolean object', :ddwaf_object_bool, true, :ddwaf_obj_bool]
-        ].each do |message, method, value, result|
+        ].each do |message, method, value, expected_object_type|
           context message do
-            it 'returns object type' do
+            it "returns object type #{expected_object_type.inspect}" do
               if value
-                libddwaf.send(method, object_for_getter_spec, value)
+                libddwaf.send(method, ddwaf_object, value)
               else
-                libddwaf.send(method, object_for_getter_spec)
+                libddwaf.send(method, ddwaf_object)
               end
-              object_type = libddwaf.ddwaf_object_type(object_for_getter_spec)
-              expect(object_type).to eq(result)
+              object_type = libddwaf.ddwaf_object_type(ddwaf_object)
+              expect(object_type).to eq(expected_object_type)
             end
           end
         end
@@ -266,12 +266,12 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
       describe '.ddwaf_object_size' do
         context 'for array object' do
           it 'returns size' do
-            libddwaf.ddwaf_object_array(object_for_getter_spec)
+            libddwaf.ddwaf_object_array(ddwaf_object)
             member_object = libddwaf::Object.new
             libddwaf.ddwaf_object_string(member_object, 'Hello World')
-            libddwaf.ddwaf_object_array_add(object_for_getter_spec, member_object)
+            libddwaf.ddwaf_object_array_add(ddwaf_object, member_object)
 
-            size = libddwaf.ddwaf_object_size(object_for_getter_spec)
+            size = libddwaf.ddwaf_object_size(ddwaf_object)
             expect(size).to eq(1)
           end
         end
@@ -279,20 +279,20 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
         context 'for map object' do
           it 'returns size' do
             key = 'foo'
-            libddwaf.ddwaf_object_map(object_for_getter_spec)
+            libddwaf.ddwaf_object_map(ddwaf_object)
             member_object = libddwaf::Object.new
             libddwaf.ddwaf_object_string(member_object, 'bar')
-            libddwaf.ddwaf_object_map_addl(object_for_getter_spec, key, key.bytesize, member_object)
+            libddwaf.ddwaf_object_map_addl(ddwaf_object, key, key.bytesize, member_object)
 
-            size = libddwaf.ddwaf_object_size(object_for_getter_spec)
+            size = libddwaf.ddwaf_object_size(ddwaf_object)
             expect(size).to eq(1)
           end
         end
 
         context 'for non container objects' do
           it 'returns 0' do
-            libddwaf.ddwaf_object_string(object_for_getter_spec, 'Hello World')
-            size = libddwaf.ddwaf_object_size(object_for_getter_spec)
+            libddwaf.ddwaf_object_string(ddwaf_object, 'Hello World')
+            size = libddwaf.ddwaf_object_size(ddwaf_object)
             expect(size).to eq(0)
           end
         end
@@ -301,16 +301,16 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
       describe '.ddwaf_object_get_string' do
         context 'for string object' do
           it 'returns string' do
-            libddwaf.ddwaf_object_string(object_for_getter_spec, 'Hello World')
-            string = libddwaf.ddwaf_object_get_string(object_for_getter_spec, libddwaf::SizeTPtr.new)
+            libddwaf.ddwaf_object_string(ddwaf_object, 'Hello World')
+            string = libddwaf.ddwaf_object_get_string(ddwaf_object, libddwaf::SizeTPtr.new)
             expect(string.get_string(0)).to eq('Hello World')
           end
         end
 
         context 'non string object' do
           it 'returns null' do
-            libddwaf.ddwaf_object_map(object_for_getter_spec)
-            string = libddwaf.ddwaf_object_get_string(object_for_getter_spec, libddwaf::SizeTPtr.new)
+            libddwaf.ddwaf_object_map(ddwaf_object)
+            string = libddwaf.ddwaf_object_get_string(ddwaf_object, libddwaf::SizeTPtr.new)
             expect(string).to be_null
           end
         end
@@ -320,22 +320,22 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
         context 'for map object' do
           before do
             key = 'foo'
-            libddwaf.ddwaf_object_map(object_for_getter_spec)
+            libddwaf.ddwaf_object_map(ddwaf_object)
             member_object = libddwaf::Object.new
             libddwaf.ddwaf_object_string(member_object, 'bar')
-            libddwaf.ddwaf_object_map_addl(object_for_getter_spec, key, key.bytesize, member_object)
+            libddwaf.ddwaf_object_map_addl(ddwaf_object, key, key.bytesize, member_object)
           end
 
           context 'with index in range' do
             it 'returns object' do
-              object = libddwaf.ddwaf_object_get_index(object_for_getter_spec, 0)
+              object = libddwaf.ddwaf_object_get_index(ddwaf_object, 0)
               expect(object).to_not be_null
             end
           end
 
           context 'with index out of range' do
             it 'returns null' do
-              object = libddwaf.ddwaf_object_get_index(object_for_getter_spec, 1)
+              object = libddwaf.ddwaf_object_get_index(ddwaf_object, 1)
               expect(object).to be_null
             end
           end
@@ -343,22 +343,22 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
 
         context 'for array object' do
           before do
-            libddwaf.ddwaf_object_array(object_for_getter_spec)
+            libddwaf.ddwaf_object_array(ddwaf_object)
             member_object = libddwaf::Object.new
             libddwaf.ddwaf_object_string(member_object, 'Hello World')
-            libddwaf.ddwaf_object_array_add(object_for_getter_spec, member_object)
+            libddwaf.ddwaf_object_array_add(ddwaf_object, member_object)
           end
 
           context 'with index in range' do
             it 'returns object' do
-              object = libddwaf.ddwaf_object_get_index(object_for_getter_spec, 0)
+              object = libddwaf.ddwaf_object_get_index(ddwaf_object, 0)
               expect(object).to_not be_null
             end
           end
 
           context 'with index out of range' do
             it 'returns null' do
-              object = libddwaf.ddwaf_object_get_index(object_for_getter_spec, 1)
+              object = libddwaf.ddwaf_object_get_index(ddwaf_object, 1)
               expect(object).to be_null
             end
           end
@@ -366,8 +366,8 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
 
         context 'non container object' do
           it 'returns null' do
-            libddwaf.ddwaf_object_string(object_for_getter_spec, 'Hello World')
-            object = libddwaf.ddwaf_object_get_index(object_for_getter_spec, 0)
+            libddwaf.ddwaf_object_string(ddwaf_object, 'Hello World')
+            object = libddwaf.ddwaf_object_get_index(ddwaf_object, 0)
             expect(object).to be_null
           end
         end
@@ -377,12 +377,12 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
         context 'for map object' do
           it 'returns object key' do
             key = 'foo'
-            libddwaf.ddwaf_object_map(object_for_getter_spec)
+            libddwaf.ddwaf_object_map(ddwaf_object)
             member_object = libddwaf::Object.new
             libddwaf.ddwaf_object_string(member_object, 'bar')
-            libddwaf.ddwaf_object_map_addl(object_for_getter_spec, key, key.bytesize, member_object)
+            libddwaf.ddwaf_object_map_addl(ddwaf_object, key, key.bytesize, member_object)
 
-            object = libddwaf.ddwaf_object_get_index(object_for_getter_spec, 0)
+            object = libddwaf.ddwaf_object_get_index(ddwaf_object, 0)
             key_object = libddwaf.ddwaf_object_get_key(object, libddwaf::SizeTPtr.new)
 
             expect(key_object.get_string(0)).to eq('foo')
@@ -390,12 +390,12 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
 
           it 'returns key length' do
             key = 'foo'
-            libddwaf.ddwaf_object_map(object_for_getter_spec)
+            libddwaf.ddwaf_object_map(ddwaf_object)
             member_object = libddwaf::Object.new
             libddwaf.ddwaf_object_string(member_object, 'bar')
-            libddwaf.ddwaf_object_map_addl(object_for_getter_spec, key, key.bytesize, member_object)
+            libddwaf.ddwaf_object_map_addl(ddwaf_object, key, key.bytesize, member_object)
 
-            object = libddwaf.ddwaf_object_get_index(object_for_getter_spec, 0)
+            object = libddwaf.ddwaf_object_get_index(ddwaf_object, 0)
             length = libddwaf::SizeTPtr.new
 
             expect(length.pointer.get_int(0)).to eq(0)
@@ -405,8 +405,8 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
 
           context 'for non map object' do
             it 'returns nulls' do
-              libddwaf.ddwaf_object_string(object_for_getter_spec, 'bar')
-              key_object = libddwaf.ddwaf_object_get_key(object_for_getter_spec, libddwaf::SizeTPtr.new)
+              libddwaf.ddwaf_object_string(ddwaf_object, 'bar')
+              key_object = libddwaf.ddwaf_object_get_key(ddwaf_object, libddwaf::SizeTPtr.new)
 
               expect(key_object).to be_null
             end
@@ -415,8 +415,8 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
 
         context 'non map objects' do
           it 'returns nulll' do
-            libddwaf.ddwaf_object_string(object_for_getter_spec, 'Hello World')
-            object = libddwaf.ddwaf_object_get_key(object_for_getter_spec, libddwaf::SizeTPtr.new)
+            libddwaf.ddwaf_object_string(ddwaf_object, 'Hello World')
+            object = libddwaf.ddwaf_object_get_key(ddwaf_object, libddwaf::SizeTPtr.new)
             expect(object).to be_null
           end
         end
@@ -425,16 +425,16 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
       describe '.ddwaf_object_get_signed' do
         context 'for signed object' do
           it 'returns value' do
-            libddwaf.ddwaf_object_signed_force(object_for_getter_spec, -12)
-            value = libddwaf.ddwaf_object_get_signed(object_for_getter_spec)
+            libddwaf.ddwaf_object_signed_force(ddwaf_object, -12)
+            value = libddwaf.ddwaf_object_get_signed(ddwaf_object)
             expect(value).to eq(-12)
           end
         end
 
         context 'for non signed object' do
           it 'returns 0' do
-            libddwaf.ddwaf_object_string(object_for_getter_spec, 'Hello World')
-            value = libddwaf.ddwaf_object_get_signed(object_for_getter_spec)
+            libddwaf.ddwaf_object_string(ddwaf_object, 'Hello World')
+            value = libddwaf.ddwaf_object_get_signed(ddwaf_object)
             expect(value).to eq(0)
           end
         end
@@ -443,16 +443,16 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
       describe '.ddwaf_object_get_unsigned' do
         context 'for unsigned object' do
           it 'returns value' do
-            libddwaf.ddwaf_object_unsigned_force(object_for_getter_spec, 12)
-            value = libddwaf.ddwaf_object_get_unsigned(object_for_getter_spec)
+            libddwaf.ddwaf_object_unsigned_force(ddwaf_object, 12)
+            value = libddwaf.ddwaf_object_get_unsigned(ddwaf_object)
             expect(value).to eq(12)
           end
         end
 
         context 'for non unsigned object' do
           it 'returns 0' do
-            libddwaf.ddwaf_object_string(object_for_getter_spec, 'Hello World')
-            value = libddwaf.ddwaf_object_get_unsigned(object_for_getter_spec)
+            libddwaf.ddwaf_object_string(ddwaf_object, 'Hello World')
+            value = libddwaf.ddwaf_object_get_unsigned(ddwaf_object)
             expect(value).to eq(0)
           end
         end
@@ -462,16 +462,16 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
         context 'for boolean object' do
           context 'true' do
             it 'returns value' do
-              libddwaf.ddwaf_object_bool(object_for_getter_spec, true)
-              value = libddwaf.ddwaf_object_get_bool(object_for_getter_spec)
+              libddwaf.ddwaf_object_bool(ddwaf_object, true)
+              value = libddwaf.ddwaf_object_get_bool(ddwaf_object)
               expect(value).to eq(true)
             end
           end
 
           context 'false' do
             it 'returns value' do
-              libddwaf.ddwaf_object_bool(object_for_getter_spec, false)
-              value = libddwaf.ddwaf_object_get_bool(object_for_getter_spec)
+              libddwaf.ddwaf_object_bool(ddwaf_object, false)
+              value = libddwaf.ddwaf_object_get_bool(ddwaf_object)
               expect(value).to eq(false)
             end
           end
@@ -479,8 +479,8 @@ RSpec.describe Datadog::AppSec::WAF::LibDDWAF do
 
         context 'for non boolean object' do
           it 'returns false' do
-            libddwaf.ddwaf_object_string(object_for_getter_spec, 'Hello World')
-            value = libddwaf.ddwaf_object_get_bool(object_for_getter_spec)
+            libddwaf.ddwaf_object_string(ddwaf_object, 'Hello World')
+            value = libddwaf.ddwaf_object_get_bool(ddwaf_object)
             expect(value).to eq(false)
           end
         end
@@ -1923,7 +1923,7 @@ RSpec.describe Datadog::AppSec::WAF do
             { 'server.request.headers.no_cookies' => { 'user-agent' => ['Nessus SOAP'] } }
           end
 
-          it 'passes' do
+          it 'passes on matching input outside of limit' do
             code, result = context.run(matching_input, timeout)
             perf_store[:total_runtime] << result.total_runtime
             expect(code).to eq :ok
@@ -1942,7 +1942,7 @@ RSpec.describe Datadog::AppSec::WAF do
             { 'server.request.headers.no_cookies' => { 'user-agent' => 'Nessus SOAP' } }
           end
 
-          it 'matches' do
+          it 'matches on matching input inside of limit' do
             code, result = context.run(matching_input, timeout)
             perf_store[:total_runtime] << result.total_runtime
             expect(code).to eq :match
