2d0cc16d9fc747ef7af122a052d4a06864c42647
Gustavo Caso <gustavo.caso@datadoghq.com>
2023-08-28 11:47:29 +0200

    Bump version to 1.10.0

--- lib/datadog/appsec/waf/version.rb
+++ lib/datadog/appsec/waf/version.rb
@@ -2,8 +2,8 @@ module Datadog
   module AppSec
     module WAF
       module VERSION
-        BASE_STRING = '1.9.0'
-        STRING = "#{BASE_STRING}.0.1"
+        BASE_STRING = '1.10.0'
+        STRING = "#{BASE_STRING}.0.0"
         MINIMUM_RUBY_VERSION = '2.1'
       end
     end
