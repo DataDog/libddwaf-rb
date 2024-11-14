4defba1f0a81f5b2d60e66197bb4ba858bd152bc
Yury Lebedev <yury.lebedev@datadoghq.com>
2024-11-14 09:49:45 +0100

    Remove verbose logging from stress test

--- spec/datadog/appsec/waf/context_stress_test_spec.rb
+++ spec/datadog/appsec/waf/context_stress_test_spec.rb
@@ -35,41 +35,6 @@ RSpec.describe Datadog::AppSec::WAF::Context, stress_tests: true do
   end
 
   let(:handle) { Datadog::AppSec::WAF::Handle.new(rule) }
-  let(:log_store) { [] }
-
-  let(:perf_store) do
-    {
-      total_runtime: []
-    }
-  end
-
-  let(:log_cb) do
-    proc do |level, func, file, line, message, len|
-      log_store << { level: level, func: func, file: file, line: line, message: message.read_bytes(len) }
-    end
-  end
-
-  before(:each) do
-    expect(perf_store).to eq({ total_runtime: [] })
-    expect(log_store).to eq([])
-
-    Datadog::AppSec::WAF::LibDDWAF.ddwaf_set_log_cb(log_cb, :ddwaf_log_trace)
-
-    expect(log_store.size).to eq 1
-    expect(log_store.select { |log| log[:message] =~ /Sending log messages to binding/ })
-  end
-
-  after(:each) do |example|
-    if example.exception
-      puts "\n== #{example.full_description}"
-      log_store.each do |log|
-        puts log.inspect
-      end
-      puts "== #{example.full_description}"
-      puts perf_store.inspect
-      puts "== #{example.full_description}"
-    end
-  end
 
   context 'stress testing' do
     let(:run_count) { 500 }
