80b71a71cd2c4f266734da2f7975eb7efa1e881b
Yury Lebedev <yury.lebedev@datadoghq.com>
2025-03-25 10:46:55 +0100

    Update libddwaf version to 1.21.0

--- lib/datadog/appsec/waf/version.rb
+++ lib/datadog/appsec/waf/version.rb
@@ -2,7 +2,7 @@ module Datadog
   module AppSec
     module WAF
       module VERSION
-        BASE_STRING = '1.20.1'
+        BASE_STRING = '1.21.0'
         STRING = "#{BASE_STRING}.0.1"
         MINIMUM_RUBY_VERSION = '2.5'
       end
