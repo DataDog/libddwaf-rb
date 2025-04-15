86a404cfca7bf4cfe119f5097f3242c5bea2ee22
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2025-04-15 12:23:56 +0200

    Bump version to 1.22.0.0.2

--- lib/datadog/appsec/waf/version.rb
+++ lib/datadog/appsec/waf/version.rb
@@ -3,7 +3,9 @@ module Datadog
     module WAF
       module VERSION
         BASE_STRING = '1.22.0'
-        STRING = "#{BASE_STRING}.0.1"
+        # NOTE: Every change to the `BASE_STRING` should be accompanied
+        #       by a reset of the patch version in the `STRING` below.
+        STRING = "#{BASE_STRING}.0.2"
         MINIMUM_RUBY_VERSION = '2.5'
       end
     end
