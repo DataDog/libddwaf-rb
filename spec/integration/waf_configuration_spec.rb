adc95b7075499f5a262059db2b69df5a3cd8bb46
Yury Lebedev <yury.lebedev@datadoghq.com>
2025-05-12 16:25:02 +0200

    Add integration specs for WAF limits configuration

---
+++ spec/integration/waf_configuration_spec.rb
@@ -0,0 +1,84 @@
+require "spec_helper"
+
+RSpec.describe "WAF configuration" do
+  let(:waf_config) do
+    JSON.parse(File.read("spec/fixtures/waf_rules.json"))
+  end
+
+  context "limits" do
+    context "max_container_size" do
+      it "matches when value is within container size limit" do
+        builder = Datadog::AppSec::WAF::HandleBuilder.new
+        builder.add_or_update_config(config: waf_config, path: "some/path")
+        handle = builder.build_handle
+        context = handle.build_context
+
+        result = context.run({"server.request.headers.no_cookies" => {"user-agent" => "Nessus SOAP"}}, {})
+
+        expect(result.status).to eq(:match)
+      end
+
+      it "does not match when value is not within container size limit" do
+        builder = Datadog::AppSec::WAF::HandleBuilder.new(limits: {max_container_size: 1})
+        builder.add_or_update_config(config: waf_config, path: "some/path")
+        handle = builder.build_handle
+        context = handle.build_context
+
+        result = context.run({"server.request.headers.no_cookies" => {"another" => "key", "user-agent" => "Nessus SOAP"}}, {})
+
+        expect(result.status).to eq(:ok)
+      end
+    end
+
+    context "max_container_depth" do
+      it "matches when value is within container depth limit" do
+        builder = Datadog::AppSec::WAF::HandleBuilder.new
+        builder.add_or_update_config(config: waf_config, path: "some/path")
+        handle = builder.build_handle
+        context = handle.build_context
+
+        result = context.run({"server.request.headers.no_cookies" => {"user-agent" => "Nessus SOAP"}}, {})
+
+        expect(result.status).to eq(:match)
+      end
+
+      it "does not match when value is not within container depth limit" do
+        builder = Datadog::AppSec::WAF::HandleBuilder.new(limits: {max_container_depth: 1})
+        builder.add_or_update_config(config: waf_config, path: "some/path")
+        handle = builder.build_handle
+        context = handle.build_context
+
+        result = context.run({"server.request.headers.no_cookies" => {"user-agent" => ["Nessus SOAP"]}}, {})
+
+        expect(result.status).to eq(:ok)
+      end
+    end
+
+    context "max_string_length" do
+      it "matches when value is within string length limit" do
+        builder = Datadog::AppSec::WAF::HandleBuilder.new(limits: {max_string_length: 11})
+        builder.add_or_update_config(config: waf_config, path: "some/path")
+        handle = builder.build_handle
+        context = handle.build_context
+
+        result = context.run({"server.request.headers.no_cookies" => {"user-agent" => "Nessus SOAP"}}, {})
+
+        expect(result.status).to eq(:match)
+      end
+
+      it "does not match when value is not within string length limit" do
+        builder = Datadog::AppSec::WAF::HandleBuilder.new(limits: {max_string_length: 10})
+        builder.add_or_update_config(config: waf_config, path: "some/path")
+        handle = builder.build_handle
+        context = handle.build_context
+
+        result = context.run({"server.request.headers.no_cookies" => {"user-agent" => ["Nessus SOAP"]}}, {})
+
+        expect(result.status).to eq(:ok)
+      end
+    end
+  end
+
+  context "obfuscator" do
+  end
+end
