7bfcf147344cfe4865c777485e79f439e1cff592
Yury Lebedev <yury.lebedev@datadoghq.com>
2025-05-13 14:31:08 +0200

    Change naming of waf rules in waf configuration spec

--- spec/integration/waf_configuration_spec.rb
+++ spec/integration/waf_configuration_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 RSpec.describe "WAF configuration" do
-  let(:waf_config) do
+  let(:waf_rules) do
     JSON.parse(File.read("spec/fixtures/waf_rules.json"))
   end
 
@@ -9,7 +9,7 @@ RSpec.describe "WAF configuration" do
     context "max_container_size" do
       it "matches when value is within container size limit" do
         builder = Datadog::AppSec::WAF::HandleBuilder.new
-        builder.add_or_update_config(waf_config, path: "some/path")
+        builder.add_or_update_config(waf_rules, path: "some/path")
         handle = builder.build_handle
         context = handle.build_context
 
@@ -20,7 +20,7 @@ RSpec.describe "WAF configuration" do
 
       it "does not match when value is not within container size limit" do
         builder = Datadog::AppSec::WAF::HandleBuilder.new(limits: {max_container_size: 1})
-        builder.add_or_update_config(waf_config, path: "some/path")
+        builder.add_or_update_config(waf_rules, path: "some/path")
         handle = builder.build_handle
         context = handle.build_context
 
@@ -33,7 +33,7 @@ RSpec.describe "WAF configuration" do
     context "max_container_depth" do
       it "matches when value is within container depth limit" do
         builder = Datadog::AppSec::WAF::HandleBuilder.new
-        builder.add_or_update_config(waf_config, path: "some/path")
+        builder.add_or_update_config(waf_rules, path: "some/path")
         handle = builder.build_handle
         context = handle.build_context
 
@@ -44,7 +44,7 @@ RSpec.describe "WAF configuration" do
 
       it "does not match when value is not within container depth limit" do
         builder = Datadog::AppSec::WAF::HandleBuilder.new(limits: {max_container_depth: 1})
-        builder.add_or_update_config(waf_config, path: "some/path")
+        builder.add_or_update_config(waf_rules, path: "some/path")
         handle = builder.build_handle
         context = handle.build_context
 
@@ -57,7 +57,7 @@ RSpec.describe "WAF configuration" do
     context "max_string_length" do
       it "matches when value is within string length limit" do
         builder = Datadog::AppSec::WAF::HandleBuilder.new(limits: {max_string_length: 11})
-        builder.add_or_update_config(waf_config, path: "some/path")
+        builder.add_or_update_config(waf_rules, path: "some/path")
         handle = builder.build_handle
         context = handle.build_context
 
@@ -68,7 +68,7 @@ RSpec.describe "WAF configuration" do
 
       it "does not match when value is not within string length limit" do
         builder = Datadog::AppSec::WAF::HandleBuilder.new(limits: {max_string_length: 10})
-        builder.add_or_update_config(waf_config, path: "some/path")
+        builder.add_or_update_config(waf_rules, path: "some/path")
         handle = builder.build_handle
         context = handle.build_context
 
@@ -82,7 +82,7 @@ RSpec.describe "WAF configuration" do
   context "obfuscator" do
     it "matches and obfuscates keys" do
       builder = Datadog::AppSec::WAF::HandleBuilder.new(obfuscator: {key_regex: "user-agent"})
-      builder.add_or_update_config(waf_config, path: "some/path")
+      builder.add_or_update_config(waf_rules, path: "some/path")
       handle = builder.build_handle
       context = handle.build_context
 
@@ -94,7 +94,7 @@ RSpec.describe "WAF configuration" do
 
     it "matches and obfuscates values" do
       builder = Datadog::AppSec::WAF::HandleBuilder.new(obfuscator: {value_regex: "SOAP"})
-      builder.add_or_update_config(waf_config, path: "some/path")
+      builder.add_or_update_config(waf_rules, path: "some/path")
       handle = builder.build_handle
       context = handle.build_context
 
