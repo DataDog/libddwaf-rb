4a1bcb943e2d47f3a951d042c5bf2c53c66d0a13
Yury Lebedev <yury.lebedev@datadoghq.com>
2025-05-16 16:49:00 +0200

    Add memory leaks spec for Converter

--- spec/memory_leaks_spec.rb
+++ spec/memory_leaks_spec.rb
@@ -8,12 +8,13 @@ RSpec.describe "Memory leaks", memory_leaks: true do
     full_waf_run.rb
     add_remote_config.rb
     handle_known_addresses.rb
+    converter.rb
   ].each do |scenario_file|
     it "does not detect memory leaks in #{scenario_file}" do
       _, stderr, status = Open3.capture3("ruby_memcheck -I lib spec/memory_leak_scenarios/#{scenario_file}")
 
       expect(status).to be_success,
-        "Expected no memory leaks in #{scenario_file}.\n\nValgrind output:\n#{stderr}"
+        "Expected no memory leaks when running #{scenario_file}.\n\nValgrind output:\n#{stderr}"
     end
   end
 end
