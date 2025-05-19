2ea820c6d427a8b4fd1198f28e8f8a694f514ed1
Yury Lebedev <yury.lebedev@datadoghq.com>
2025-05-19 17:50:57 +0200

    Add scenarios for running with persistent and ephemeral data

---
+++ spec/memory_leaks/run_waf_with_persistent_data_scenario.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "libddwaf"
+require "json"
+
+builder = Datadog::AppSec::WAF::HandleBuilder.new
+
+config = JSON.parse(File.read("spec/fixtures/valid_config.json"))
+builder.add_or_update_config(config, path: "APPSEC/RC_CONFIG")
+
+handle = builder.build_handle
+context = handle.build_context
+
+context.run({"server.request.query" => {"user_id" => "foo';delete from users;--"}}, {})
+
+context.finalize!
+handle.finalize!
+builder.finalize!
