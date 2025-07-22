5be95a96af591c9bfaf568ec77f6b55cd6cc174c
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2025-07-22 15:13:14 +0200

    Bump version to v1.22.0.0.4
    * Port of #83

--- lib/datadog/appsec/waf/version.rb
+++ lib/datadog/appsec/waf/version.rb
@@ -5,7 +5,7 @@ module Datadog
         BASE_STRING = '1.22.0'
         # NOTE: Every change to the `BASE_STRING` should be accompanied
         #       by a reset of the patch version in the `STRING` below.
-        STRING = "#{BASE_STRING}.0.3"
+        STRING = "#{BASE_STRING}.0.4"
         MINIMUM_RUBY_VERSION = '2.5'
       end
     end
